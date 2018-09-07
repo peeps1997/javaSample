78d201040e506f2360474a0a37816ebec13f4ea2
Abhishek <ashekhawat@teksystems.com>
2018-09-07 18:56:27 +0530

    "minor tweaks"

--- world/src/openWorld/DamageMeta.java
+++ world/src/openWorld/DamageMeta.java
@@ -6,6 +6,21 @@ import players.Child;
 
 public class DamageMeta implements Comparator<Child>{
 	public boolean free=true;
+	public boolean isFree() {
+		return free;
+	}
+	public boolean isDmg() {
+		return isDmg;
+	}
+	public int getDmg() {
+		return dmg;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public String getRec() {
+		return rec;
+	}
 	public boolean isDmg=false;
 	public int dmg;
 	public String author;
